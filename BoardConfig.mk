6ae0221bd823143d75f8a5a2db082e484080a83a
xoomdev <xoomdevnet@gmail.com>
2012-09-02 17:54:14 -0400

    grouper: Add TARGET_NO_BOOTLOADER := true
    Change-Id: I2ec93d18fa48af973744c4bbdf31e6a50a237fa3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,7 @@ WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 
+TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := grouper
 TARGET_BOARD_INFO_FILE := device/asus/grouper/board-info.txt
 
