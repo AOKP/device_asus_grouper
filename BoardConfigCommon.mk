6bf392d197612ed64b6ff2953e8110433517c3de
xoomdev <xoomdevnet@gmail.com>
2012-11-16 08:58:10 -0600

    Add target_no_bootloader

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,6 +57,7 @@ WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 
 TARGET_BOOTLOADER_BOARD_NAME := grouper
+TARGET_NO_BOOTLOADER := true
 
 BOARD_USES_GENERIC_AUDIO := false
 BOARD_USES_ALSA_AUDIO := false
