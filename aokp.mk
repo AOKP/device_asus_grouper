e0aa39397aa160d721c4e1812566b25c435463e8
srisurya95 <srisurya95@gmail.com>
2016-02-24 05:58:21 -0600

    Aokp-ify

--- aokp.mk
+++ aokp.mk
@@ -5,18 +5,18 @@ PRODUCT_RELEASE_NAME := Nexus7
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common aokp stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/grouper/full_grouper.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grouper
-PRODUCT_NAME := cm_grouper
+PRODUCT_NAME := aokp_grouper
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
